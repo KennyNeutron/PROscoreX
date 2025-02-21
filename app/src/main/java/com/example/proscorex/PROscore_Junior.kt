2e74536f87658a85ff2590b40dad5f1632f180b4
KennyNeutron <kennyneutron.hex@gmail.com>
2025-02-21 14:07:55 +0800

    timer is now working in proscore junior

--- app/src/main/java/com/example/proscorex/PROscore_Junior.kt
+++ app/src/main/java/com/example/proscorex/PROscore_Junior.kt
@@ -4,8 +4,13 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
 import android.widget.TextView
+import android.os.CountDownTimer
 
 class PROscore_Junior : AppCompatActivity() {
+
+    private lateinit var  timerTextView: TextView
+    private var timer: CountDownTimer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_proscore_junior)  // Ensure you have a corresponding layout file
@@ -15,6 +20,9 @@ class PROscore_Junior : AppCompatActivity() {
         val lbl_HomeFoulValue = findViewById<TextView>(R.id.lbl_Home_Foul_Value)
 
         var int_HomeFoul = lbl_HomeFoulValue.text.toString().toInt()
+
+        timerTextView = findViewById(R.id.lbl_GameTime)
+        startTimer(10 * 60 * 1000)
 
         btn_HomeFoulAdd.setOnClickListener{
             int_HomeFoul++
@@ -61,7 +69,26 @@ class PROscore_Junior : AppCompatActivity() {
                 lbl_GuestFoulValue.text = int_GuestFoul.toString()
             }
         }
-        
+    }
+
+    private fun startTimer(timeInMillis: Long) {
+        timer = object : CountDownTimer(timeInMillis, 100) { // update every 100ms
+            override fun onTick(millisUntilFinished: Long) {
+                val minutes = (millisUntilFinished / 1000) / 60
+                val seconds = (millisUntilFinished / 1000) % 60
+                val millis = (millisUntilFinished % 1000) / 100
+                timerTextView.text = String.format("%02d:%02d.%1d", minutes, seconds, millis)
+            }
+
+            override fun onFinish() {
+                timerTextView.text = "00:00:0"
+            }
+        }.start()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        timer?.cancel() // Stop the timer to avoid memory leaks
     }
 
 }
